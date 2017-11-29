c4021db900d6faf58222e61b491a758027686d13
Hamish McKenzie-Campbell <geitaguy@gmail.com>
2017-11-29 18:02:26 +0800

    Updated 29/11/17

--- transmission-eztv.rb
+++ transmission-eztv.rb
@@ -1,9 +1,8 @@
 class TransmissionEztv < Formula
-  version "20171009"
+  version "20171129"
   desc "Integrate EZTV with Transmission BitTorrent client"
   homepage "https://github.com/geitaguy/transmission-eztv"
   url "https://github.com/geitaguy/transmission-eztv.git"
-  sha256 "24d89bc2d99c645f2bdc4d5a35b58a8cff1b50a995dff0f69dfbb7e58d9a48a8"
 
   depends_on "transmission" => :recommended
   depends_on "curl" => "with-c-ares"
